4c6c8b6dc2d4593c16205d5feec782a89c093e4e
pattenbranden <patten.branden@gmail.com>
2020-03-17 18:05:00 -0400

    added push route

--- data/userData.js
+++ data/userData.js
@@ -1,13 +1,24 @@
-var users = [
+var usersArray = [
     {
-        Name: "Branden",
-        Age: 26,
-        qAnswers: [2,1,1]
+        photoURL: "https://picsum.photos/200",
+        name: "Branden",
+        age: 26,
+        a1: 5,
+        a2: 4,
+        a3: 3,
+        a4: 2,
+        a5: 1,
     },
     {
-        Name: "Brooke",
-        Age: 26,
-        qAnswers: [1,2,1]
+        photoURL: "https://picsum.photos/200",
+        name: "Brooke",
+        age: 26,
+        a1: 5,
+        a2: 4,
+        a3: 3,
+        a4: 2,
+        a5: 1,
     },
 
 ]
+module.exports = usersArray
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,8 +3,10 @@ var path = require("path");
 // var results = require("../data/resultsData")
 module.exports = function(app){
     //post the results
-    app.post("/api/results/:", function(req,res){
-
+    app.post("/api/results", function(req,res){
+        users.push(req.body)
+        console.log(req.body)
+        res.sendFile(__dirname, "../public/results.html");
     })
     //
 }
