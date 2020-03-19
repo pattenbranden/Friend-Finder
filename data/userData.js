43f78805b335edabd6eb5d1b0259bfdaf0c74f88
pattenbranden <patten.branden@gmail.com>
2020-03-18 22:03:54 -0400

    updated some logic for the presentation

--- data/userData.js
+++ data/userData.js
@@ -1,24 +1,116 @@
 var usersArray = [
     {
-        photoURL: "https://picsum.photos/200",
-        name: "Branden",
-        age: 26,
-        a1: 5,
-        a2: 4,
-        a3: 3,
-        a4: 2,
-        a5: 1,
+        "name": "Barack Obama",
+        "photo": "https://s.yimg.com/ny/api/res/1.2/xqweg9GZJsh0aC1CMRBqJg--/YXBwaWQ9aGlnaGxhbmRlcjt3PTcwNTtoPTQ5Ny4wODAwNzgxMjU-/https://s.yimg.com/uu/api/res/1.2/6MizxcYxJdxXKwnQchKUfg--~B/aD0xNDQ0O3c9MjA0ODtzbT0xO2FwcGlkPXl0YWNoeW9u/https://media-mbst-pub-ue1.s3.amazonaws.com/creatr-uploaded-images/2020-03/a677f200-6502-11ea-bfb9-9a16389b4939",
+        "scores": [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+        ]
     },
     {
-        photoURL: "https://picsum.photos/200",
-        name: "Brooke",
-        age: 26,
-        a1: 5,
-        a2: 4,
-        a3: 3,
-        a4: 2,
-        a5: 1,
+        "name": "Joe Biden",
+        "photo": "https://www.biography.com/.image/c_fit%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_620/MTQwODQ0Nzc0MjIyNDczMDA2/joe-biden-official-portrait_1600jpg.jpg",
+        "scores": [
+        "4",
+        "2",
+        "5",
+        "1",
+        "3",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2"
+        ]
     },
+    {
+        "name": "Jennifer Lawrence",
+        "photo": "https://media1.popsugar-assets.com/files/thumbor/i63Jb8lKHjsRWeu4a9IKpAHZ1go/364x0:4636x4272/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2019/08/28/717/n/1922398/274cf0545d66a7fd9b2956.87657545_/i/Jennifer-Lawrence.jpg",
+        "scores": [
+        "5",
+        "2",
+        "2",
+        "2",
+        "4",
+        "1",
+        "3",
+        "2",
+        "5",
+        "5"
+        ]
+    },
+    {
+        "name": "Kelly Clarkson",
+        "photo": "https://api.time.com/wp-content/uploads/2015/04/kelly-clarkson.jpg",
+        "scores": [
+        "3",
+        "3",
+        "4",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2",
+        "2",
+        "3"
+        ]
+    },
+    {
+        "name": "Scarlett Johansson",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/6/60/Scarlett_Johansson_by_Gage_Skidmore_2_%28cropped%29.jpg",
+        "scores": [
+        "4",
+        "3",
+        "4",
+        "1",
+        "5",
+        "2",
+        "5",
+        "3",
+        "1",
+        "4"
+        ]
+    },
+    {
+        "name": "Toby Keith",
+        "photo": "https://www.bing.com/th?id=OIP.yENHcgwFyzjendCGk0fo_QHaHa&pid=Api&rs=1",
+        "scores": [
+        "4",
+        "4",
+        "2",
+        "3",
+        "2",
+        "2",
+        "3",
+        "2",
+        "4",
+        "5"
+        ]
+    },
+    {
+        "name": "Guy Ferarri",
+        "photo": "https://pbs.twimg.com/profile_images/991380156013920256/PZRGsMjW.jpg",
+        "scores": [
+        "5",
+        "5",
+        "5",
+        "2",
+        "1",
+        "4",
+        "3",
+        "3",
+        "5",
+        "5"
+        ]
+    }
 
 ]
-module.exports = usersArray
+module.exports = usersArray;
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,15 +1,44 @@
 var users = require("../data/userData");
 var path = require("path");
+
 // var results = require("../data/resultsData")
-module.exports = function(app){
+module.exports = function (app) {
     //post the results
-    app.post("/api/results", function(req,res){
-        users.push(req.body)
-        console.log(req.body)
-        res.sendFile(__dirname, "../public/results.html");
-    })
+    // app.post("/api/results", function(req,res){
+    //     users.push(req.body)
+    //     console.log(req.body)
+    //     res.sendFile(__dirname, "../public/results.html");
+    // })
     //
+    app.post("/api/userData", function (req, res) {
+        var newUser = req.body;
+        var newUserScores = req.body.scores;
+        var absArr = [];
+        console.log(newUser)
+        for (var i = 0; (users.length - 1); i++) {
+            console.log("________________________________________\n" + users[i].name + "\n" + users[i].photo)
+            var databaseUserScores = users[i].scores;
+            var difference = 0;
+            for (var j = 0; j < databaseUserScores.length; j++) {
+                difference += (Math.abs(databaseUserScores[j] - newUserScores[j]))
+            }
+            absArr.push(difference);
+            console.log("________________________________________\n")
+        }
+        var lowestAbs = Math.min(...absArr);
+        var match = absArr.indexOf(lowestAbs);
+
+        res.json(users[match]);
+        users.push(newUser);
+
+    })
+
+    app.get("/api/userData", function (req, res) {
+        res.json(users);
+    })
+
 }
+
 
 //route to test questions
 //route to restults
